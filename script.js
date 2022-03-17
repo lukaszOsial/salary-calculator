e957ab36cf30ff1419156f62907a7eb6a6266391
lukaszOsial <lukasz.osial@gmail.comq$git config --global user.email lukasz.osial@gmail.com>
2022-03-17 19:05:31 +0100

    add new funciotn

---
+++ script.js
@@ -0,0 +1,5 @@
+const salaryButton = document.querySelector(".button-contract-of-employment");
+salaryButton.addEventListener("click", () =>{
+    let salary = document.getElementById('salary').value;
+    console.log(salary);
+});
