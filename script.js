307ad344d0d2c8685bce52d677299f6be7b13770
lukaszOsial <lukasz.osial@gmail.comq$git config --global user.email lukasz.osial@gmail.com>
2022-03-18 11:30:35 +0100

    project expansion

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const salaryButton = document.querySelector(".button-contract-of-employment");
 salaryButton.addEventListener("click", () =>{
+    getSalary();
+    getFirstForm();
+});
+
+function getSalary(){
     let salary = document.getElementById('salary').value;
     console.log(salary);
-});
+}
+function getFirstForm(){
+    const first = document.getElementsByName('first');
+    let checked = [];
+    for(let i = 0; i < first.length; i++){
+    if(first[i].checked){
+        checked += ' ' + first[i].value;
+        }
+    }
+    console.log(checked);
+}
